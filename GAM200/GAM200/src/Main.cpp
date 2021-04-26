7c80387d7bf02545f1d614e821aae0a01253162b
Felicia <weilingfelicia.tan@digipen.edu>
2021-04-26 23:01:55 +0800

    added Main.cpp and Main.hpp

--- GAM200/GAM200/src/Main.cpp
+++ GAM200/GAM200/src/Main.cpp
@@ -1,16 +1,16 @@
 /* Start Header**********************************************************************************/
 /*!
-\file EntityManager.hpp
-\team name 
-\software name 
+\file Main.cpp
+\team name
+\software name
 \authors
-NAME							EMAIL									ROLE	
+NAME							EMAIL									ROLE
 Tan Wei Ling Felicia			weilingfelicia.tan@digipen.edu			PRODUCER
 Goh	See Yong Denise				2001220@sit.singaporetech.edu.sg
 
 Ow Jian Wen						jianwen123321@hotmail.com				TECHINCAL DIRECTOR
 Chia Yi Da						chiayida98@gmail.com
-Margaret Teo Boon See			Teo.b@digipen.edu	
+Margaret Teo Boon See			Teo.b@digipen.edu
 
 Wang Ao							Ao.Wang@digipen.edu
 Ng Jia Yi						Jiayi.ng@digipen.edu
@@ -20,8 +20,7 @@ Ng Jia Yi						Jiayi.ng@digipen.edu
 \date 26/04/2021
 \brief
 
-Entity Manager is in charge of assigning entity IDs and keeping records of which IDs are in use
-and not. 
+
 
 
 Copyright (C) 2021 DigiPen Institute of Technology.
@@ -30,15 +29,3 @@ without the prior written consent of DigiPen Institute of
 Technology is prohibited.
 */
 /* End Header **********************************************************************************/
-
-#pragma once
-
-class EntityManager
-{
-	public:
-		EntityManager()
-		{
-			
-		}
-};
-

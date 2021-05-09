9d1666a0d873ea5f7f4a471e146761e9b209cdcc
Felicia <weilingfelicia.tan@digipen.edu>
2021-05-09 23:05:31 +0800

    Created ComponentManager.hpp
    Have yet to begin, am trying to understand how it works

---
+++ GAM200/src/Header/Component/ComponentManager.hpp
@@ -0,0 +1,29 @@
+/* Start Header**********************************************************************************/
+/*!
+\file ComponentManager.hpp
+\team name
+\software name
+\authors
+NAME							EMAIL									ROLE
+Tan Wei Ling Felicia			weilingfelicia.tan@digipen.edu			PRODUCER
+Goh	See Yong Denise				2001220@sit.singaporetech.edu.sg
+Ow Jian Wen						jianwen123321@hotmail.com				TECHINCAL DIRECTOR
+Chia Yi Da						chiayida98@gmail.com
+Margaret Teo Boon See			Teo.b@digipen.edu
+Wang Ao							Ao.Wang@digipen.edu
+Ng Jia Yi						Jiayi.ng@digipen.edu
+\date 26/04/2021
+\brief
+
+A component manager acts as an in charge of handling the different Component Array whenever a 
+component has tpo be added or removed. It contains Component Type, RegisterComponent. it also has
+a unique key which is a map to ComponentArray to the ComponentType
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **********************************************************************************/
+#pragma once
+

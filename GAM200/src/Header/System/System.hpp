a7e2d9d342f4c6e09c3185067a9181df3b2a76de
Felicia <weilingfelicia.tan@digipen.edu>
2021-05-22 19:51:34 +0800

    Added 2 new files

---
+++ GAM200/src/Header/System/System.hpp
@@ -0,0 +1,39 @@
+/* Start Header**********************************************************************************/
+/*!
+\file System.hpp
+\team name
+\software name
+\authors
+NAME							EMAIL									ROLE
+Tan Wei Ling Felicia			weilingfelicia.tan@digipen.edu			PRODUCER
+Goh	See Yong Denise				2001220@sit.singaporetech.edu.sg
+Ow Jian Wen						jianwen123321@hotmail.com				TECHINCAL DIRECTOR
+Chia Yi Da						chiayida98@gmail.com
+Margaret Teo Boon See			Teo.b@digipen.edu
+Wang Ao							Ao.Wang@digipen.edu
+Ng Jia Yi						Jiayi.ng@digipen.edu
+\date 26/04/2021
+\brief
+
+This is to allow iteration in a list of entities with a certain signature of components
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **********************************************************************************/
+
+#pragma once
+
+#include "../Global.hpp"
+#include <set> //associative container that contains a sorted set of unique object of type Key
+
+class System
+{
+	public:
+		std::set<Entity> mEntities;
+
+};
+
+

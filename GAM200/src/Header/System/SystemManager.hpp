60ef0b0c2c379353ad6d76d0f6b631a2fc21ecb3
Felicia <weilingfelicia.tan@digipen.edu>
2021-05-22 22:36:49 +0800

    Completed system mg

--- GAM200/src/Header/System/SystemManager.hpp
+++ GAM200/src/Header/System/SystemManager.hpp
@@ -6,7 +6,7 @@
 \authors
 NAME							EMAIL									ROLE
 Tan Wei Ling Felicia			weilingfelicia.tan@digipen.edu			PRODUCER
-Goh	See Yong Denise				2001220@sit.singaporetech.edu.sg
+Goh	See Yong Denise				2001220@sit.singaporetech.edu.sg		
 Ow Jian Wen						jianwen123321@hotmail.com				TECHINCAL DIRECTOR
 Chia Yi Da						chiayida98@gmail.com
 Margaret Teo Boon See			Teo.b@digipen.edu
@@ -15,7 +15,8 @@ Ng Jia Yi						Jiayi.ng@digipen.edu
 \date 26/04/2021
 \brief
 
-A class System to set entity
+Maintining a record of registered systems and the signatures. Each system is to have a set of 
+signature to allow the manager to add the correct entities to the system's list
 
 Copyright (C) 2021 DigiPen Institute of Technology.
 Reproduction or disclosure of this file or its contents
@@ -34,5 +35,69 @@ Technology is prohibited.
 
 class SystemManager
 {
+	public:
+		template<typename T>
+		std::shared_ptr<T> SystemReg()
+		{
+			const char* TypeName = typeid(T).name();
+
+			//error checking
+			assert(Systems.find(TypeName) == Systems.end() && "Register system more than once.");
+
+			//ptr to system for return value
+			auto system = std::make_shared<T>();
+			Systems.insert({ TypeName, system });
+
+			return system;
+		}
+
+		template<typename T>
+		void SetSignature(Signature signature)
+		{
+			const char* TypeName = typeid(T).name();
+
+			//error checking
+			assert(Systems.find(TypeName) != Systems.end() && "Used before register.");
+
+			// Set the signature for this system
+			mSignatures.insert({ TypeName, signature });
+		}
+
+		void EntityDestroyed(Entity entity)
+		{
+			// Erase destroyed entity from all system lists
+			for (auto const& sys : Systems)
+			{
+				auto const& system = sys.second;
+
+				system->mEntities.erase(entity);
+			}
+		}
+
+		void EntitySignatureChanged(Entity entity, Signature entitySignature)
+		{
+			// Notify each system that an entity's signature changed
+			for (auto const& sys : Systems)
+			{
+				auto const& Type = sys.first;
+				auto const& systemptr = sys.second;
+				auto const& systemSignature = Signatures[Type];
+
+				// Entity signature matches system signature 
+				if ((entitySignature & systemSignature) == systemSignature)
+				{
+					systemptr->mEntities.insert(entity);
+				}
+				// Entity signature does not match system signature 
+				else
+				{
+					systemptr->mEntities.erase(entity);
+				}
+			}
+		}
+
+	private:
+		std::unordered_map<const char*, Signature> Signatures{}; //system typ->signature
+		std::unordered_map<const char*, std::shared_ptr<System>> Systems{}; //system ptr->system ptr
 
 };

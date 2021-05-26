df941ebeeb927b8946ddc2d4113c7d05fd34657d
Felicia <weilingfelicia.tan@digipen.edu>
2021-05-26 22:01:58 +0800

    Remove Matrix, Update Vector and Transform

--- GAM200/src/Header/Math/Vector.hpp
+++ GAM200/src/Header/Math/Vector.hpp
@@ -15,10 +15,7 @@ Ng Jia Yi						Jiayi.ng@digipen.edu
 \date 26/04/2021
 \brief
 
-To create a data structure that is a simple array with no holes. It contains the mapping from
-entity IDs to array indices. When accessing array, it uses entity ID to look up the actual array index
-but when destroyed, it takes the last elemet of array and move into deleted entity's spott and update
-the map
+Vectors and this supports 2D and 3D vectors
 
 Copyright (C) 2021 DigiPen Institute of Technology.
 Reproduction or disclosure of this file or its contents
--- GAM200/src/Header/TransformComponent.hpp
+++ GAM200/src/Header/TransformComponent.hpp
@@ -1,9 +1,15 @@
 #ifndef TRANSFORM_COMPONENT_H
 #define TRANSFORM_COMPONENT_H
 
+#include "../Header/Math/Vector.hpp"
+
 struct Transform {
 	float x, y, z, 
 		scaleX, scaleY, scaleZ;
+
+	VectorND position;
+	VectorND rotate;
+	VectorND scale;
 };
 
 #endif

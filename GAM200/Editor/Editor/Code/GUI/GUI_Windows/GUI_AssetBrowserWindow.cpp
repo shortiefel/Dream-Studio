8b711b4a6bdd0ffee4bf2e6e193e513fa378b6e8
gsydenise <2001220@sit.singaporetech.edu.sg>
2021-10-29 10:53:50 +0800

    Fixed the freetype dll extension issue
    working on asset manager

--- GAM200/Editor/Editor/Code/GUI/GUI_Windows/GUI_AssetBrowserWindow.cpp
+++ GAM200/Editor/Editor/Code/GUI/GUI_Windows/GUI_AssetBrowserWindow.cpp
@@ -35,54 +35,6 @@ namespace Editor {
 		};
 
 		static AssetView _currentView = AssetView::SceneBrowser;
-		//static void ShowSceneBrowser()
-		//{
-		//	if (_currentDirectory != std::filesystem::path(_assetPath))
-		//	{
-		//		if (ImGui::Button("Assets"))
-		//		{
-		//			_currentDirectory = _currentDirectory.parent_path();
-		//		}
-		//	}
-		//	//int sceneCount = 0;
-		//	for (auto& directory : std::filesystem::directory_iterator("Assets/Scenes"))
-		//	{
-		//		const auto& path = directory.path();
-		//		auto relative_path = std::filesystem::relative(path, "Assets/Scenes");
-		//		std::string filenameString = relative_path.string();
-
-		//		ImGui::PushID(filenameString.c_str());
-		//		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
-		//		ImGui::ImageButton((ImTextureID)directory.is_character_file(), { 128, 128 }, { 0, 1 }, { 1, 0 });
-		//		ImGui::PopStyleColor();
-		//		//ImGui::PushID(sceneCount++);
-		//		if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
-		//		{
-		//			if (filenameString == "Assets/Scenes/test2.scene")
-		//			{
-		//				Engine::SceneManager::GetInstance().ChangeScene("test2");
-		//			}
-
-		//			else if (relative_path.string() == "Scenes/test3.scene")
-		//			{
-		//				Engine::SceneManager::GetInstance().ChangeScene("test3");
-		//			}
-
-		//			else
-		//			{
-		//				Engine::SceneManager::GetInstance().ChangeScene("test1");
-		//			}
-
-		//		}
-		//		ImGui::TextWrapped(filenameString.c_str());
-
-		//		ImGui::NextColumn();
-
-		//		ImGui::PopID();
-
-		//	}
-		//	ImGui::Columns(1);
-		//}
 
 		void GUI_AssetBrowser(bool* asset_bool, ImGuiWindowFlags window_flags)
 		{
@@ -94,25 +46,6 @@ namespace Editor {
 				ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImGui::GetStyleColorVec4(ImGuiCol_FrameBgHovered));
 				ImGui::PushStyleColor(ImGuiCol_Text, ImGui::GetStyleColorVec4(ImGuiCol_Text));
 				ImGui::PushStyleColor(ImGuiCol_TextDisabled, ImGui::GetStyleColorVec4(ImGuiCol_TextDisabled));
-
-				//switch (_currentView)
-				//{
-				//	case AssetView::TextureBrowser:
-				//		ShowTextureBrowser();
-				//		break;
-				//case AssetView::SceneBrowser:
-				//	ShowSceneBrowser();
-				//	break;
-				//	case AssetView::ScriptBrowser:
-				//		ShowScriptBrowser();
-				//		break;
-				//	case AssetView::FontBrowser:
-				//		ShowFontBrowser();
-				//		break;
-				//default:
-				//	Log::Warning("Unkown asset view: %d", (int)m_CurrentView);
-				//	break;
-				//}
 
 				ImGui::PopStyleColor(4);
 

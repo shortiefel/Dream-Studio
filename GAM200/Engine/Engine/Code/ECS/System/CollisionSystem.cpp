1b211608c2794c89f9fd64cfd0da45db1558fbaa
Jw <jianwen123321@hotmail.com>
2021-11-04 01:45:51 +0800

    New fixed delta time

--- GAM200/Engine/Engine/Code/ECS/System/CollisionSystem.cpp
+++ GAM200/Engine/Engine/Code/ECS/System/CollisionSystem.cpp
@@ -38,6 +38,8 @@ Technology is prohibited.
 
 #include "Engine/Header/Physics/Ray.hpp" //Ray
 
+#include "Engine/Header/Time/DeltaTime.hpp"
+
 namespace Engine {
 	std::unordered_map<Entity_id, std::vector<Entity>> overlapMap;
 
@@ -79,116 +81,119 @@ namespace Engine {
 	void CollisionSystem::Update(float) {
 		PROFILER_START("Collision");
 
-		auto& colliderArray = dreamECSGame->GetComponentArrayData<ColliderComponent>();
-		auto colliderStart = colliderArray.begin(),
-			colliderEnd = colliderArray.end();
-		for (auto& col1 = colliderStart; col1 < colliderEnd; col1++) {
-			const Entity_id& entity_id1 = col1->GetEntityId();
-			if (EntityId_Check(entity_id1)) break;
-			if (!col1->isActive) continue;
+		int totalStep = DeltaTime::GetInstance().GetNumberOfSteps();
+		for (int step = 0; step < totalStep; step++) {
+			auto& colliderArray = dreamECSGame->GetComponentArrayData<ColliderComponent>();
+			auto colliderStart = colliderArray.begin(),
+				colliderEnd = colliderArray.end();
+			for (auto& col1 = colliderStart; col1 < colliderEnd; col1++) {
+				const Entity_id& entity_id1 = col1->GetEntityId();
+				if (EntityId_Check(entity_id1)) break;
+				if (!col1->isActive) continue;
 
-			auto& transform1 = dreamECSGame->GetComponent<TransformComponent>(entity_id1);
-			if (!transform1.isActive) continue;
+				auto& transform1 = dreamECSGame->GetComponent<TransformComponent>(entity_id1);
+				if (!transform1.isActive) continue;
 
-			ColliderComponent collider1 = *col1;
-			collider1.offset_position += Math::vec2{ transform1.position };
-			collider1.offset_scale *= transform1.scale;
-			collider1.angle += transform1.angle;
+				ColliderComponent collider1 = *col1;
+				collider1.offset_position += Math::vec2{ transform1.position };
+				collider1.offset_scale *= transform1.scale;
+				collider1.angle += transform1.angle;
 
-			/*
-			* Start of collision checks
-			*/
-			for (auto col2 = col1; col2 < colliderEnd; col2++) {
-				const Entity_id& entity_id2 = col2->GetEntityId();
-				if (EntityId_Check(entity_id2)) break;
-				if (!col2->isActive || col1 == col2) continue;
-				const RigidBodyComponent* rb1 = dreamECSGame->GetComponentPTR<RigidBodyComponent>(entity_id1);
-				const RigidBodyComponent* rb2 = dreamECSGame->GetComponentPTR<RigidBodyComponent>(entity_id2);
-				bool ent1IsMoveable = (rb1 != nullptr) && rb1->isActive,
-					ent2IsMoveable = (rb2 != nullptr) && rb2->isActive;
-				if (!ent1IsMoveable && !ent2IsMoveable) continue;
+				/*
+				* Start of collision checks
+				*/
+				for (auto col2 = col1; col2 < colliderEnd; col2++) {
+					const Entity_id& entity_id2 = col2->GetEntityId();
+					if (EntityId_Check(entity_id2)) break;
+					if (!col2->isActive || col1 == col2) continue;
+					const RigidBodyComponent* rb1 = dreamECSGame->GetComponentPTR<RigidBodyComponent>(entity_id1);
+					const RigidBodyComponent* rb2 = dreamECSGame->GetComponentPTR<RigidBodyComponent>(entity_id2);
+					bool ent1IsMoveable = (rb1 != nullptr) && rb1->isActive,
+						ent2IsMoveable = (rb2 != nullptr) && rb2->isActive;
+					if (!ent1IsMoveable && !ent2IsMoveable) continue;
 
-				auto& transform2 = dreamECSGame->GetComponent<TransformComponent>(entity_id2);
-				if (!transform2.isActive) continue;
+					auto& transform2 = dreamECSGame->GetComponent<TransformComponent>(entity_id2);
+					if (!transform2.isActive) continue;
 
-				if (transform1.layer != transform2.layer) continue; //collision layer
+					if (transform1.layer != transform2.layer) continue; //collision layer
 
-				ColliderComponent collider2 = *col2;
-				collider2.offset_position += Math::vec2{ transform2.position };
-				collider2.offset_scale *= transform2.scale;
-				collider2.angle += transform2.angle;
+					ColliderComponent collider2 = *col2;
+					collider2.offset_position += Math::vec2{ transform2.position };
+					collider2.offset_scale *= transform2.scale;
+					collider2.angle += transform2.angle;
 
-				//Direction from collider2 towards collider1
-				Math::vec2 dir = Math::vec2{};
+					//Direction from collider2 towards collider1
+					Math::vec2 dir = Math::vec2{};
 
-				if (CollisionImplementation::isColliding(dir, collider1, ent1IsMoveable,
-					collider2, ent2IsMoveable)) {
+					if (CollisionImplementation::isColliding(dir, collider1, ent1IsMoveable,
+						collider2, ent2IsMoveable)) {
 
-					AddOverlap(entity_id1, collider1.isTrigger, entity_id2, collider2.isTrigger);
-					AddOverlap(entity_id2, collider2.isTrigger, entity_id1, collider1.isTrigger);
+						AddOverlap(entity_id1, collider1.isTrigger, entity_id2, collider2.isTrigger);
+						AddOverlap(entity_id2, collider2.isTrigger, entity_id1, collider1.isTrigger);
 
-					if (collider1.isTrigger || collider2.isTrigger) {
-						LOG_INFO("Trigger");
-						continue;
+						if (collider1.isTrigger || collider2.isTrigger) {
+							LOG_INFO("Trigger");
+							continue;
+						}
+
+						//A bit of a hack
+						//To prevent object from sharing the same position
+						//by moving it slightly out of each other and setting a direction
+						//as previous direction would be dividing by 0
+						Math::vec2 EpsilonCheck = collider2.offset_position - collider1.offset_position;
+						if (EpsilonCheck.x < Math::epsilon<float>() && EpsilonCheck.x > -Math::epsilon<float>() &&
+							EpsilonCheck.y < Math::epsilon<float>() && EpsilonCheck.y > -Math::epsilon<float>()) {
+
+							collider1.offset_position.x -= 0.5f * transform1.scale.x;
+							transform1.position.x -= 0.5f * transform1.scale.x;
+							dir = Math::vec2{ 1.f, 0.f };
+						}
+						CollisionImplementation::CollisionResolution(dir, transform1, collider1, transform2, collider2);
 					}
 
-					//A bit of a hack
-					//To prevent object from sharing the same position
-					//by moving it slightly out of each other and setting a direction
-					//as previous direction would be dividing by 0
-					Math::vec2 EpsilonCheck = collider2.offset_position - collider1.offset_position;
-					if (EpsilonCheck.x < Math::epsilon<float>() && EpsilonCheck.x > -Math::epsilon<float>() &&
-						EpsilonCheck.y < Math::epsilon<float>() && EpsilonCheck.y > -Math::epsilon<float>()) {
-						
-						collider1.offset_position.x -= 0.5f * transform1.scale.x;
-						transform1.position.x -= 0.5f * transform1.scale.x;
-						dir = Math::vec2{ 1.f, 0.f };
-					}
-					CollisionImplementation::CollisionResolution(dir, transform1, collider1, transform2, collider2);
-				}
+					else {
+						const auto& iter1 = overlapMap.find(entity_id1);
+						if (iter1 != overlapMap.end()) {
+							size_t size1 = iter1->second.size();
+							for (size_t i = 0; i < size1; i++) {
+								if (iter1->second[i].id == entity_id2) {
+									overlapMap[entity_id1].erase(iter1->second.begin() + i);
 
-				else {
-					const auto& iter1 = overlapMap.find(entity_id1);
-					if (iter1 != overlapMap.end()) {
-						size_t size1 = iter1->second.size();
-						for (size_t i = 0; i < size1; i++) {
-							if (iter1->second[i].id == entity_id2) {
-								overlapMap[entity_id1].erase(iter1->second.begin() + i);
+									MonoFunctionType type;
+									if (collider1.isTrigger)
+										type = MonoFunctionType::TRIGGER_EXIT;
+									else
+										type = MonoFunctionType::COLLISION_EXIT;
 
-								MonoFunctionType type;
-								if (collider1.isTrigger)
-									type = MonoFunctionType::TRIGGER_EXIT;
-								else
-									type = MonoFunctionType::COLLISION_EXIT;
+									OverlapColliderEvent event(entity_id1, entity_id2, type);
+									EventDispatcher::SendEvent(event);
+									break;
+								}
+							}
+						}
+						const auto& iter2 = overlapMap.find(entity_id2);
+						if (iter2 != overlapMap.end()) {
+							size_t size2 = iter2->second.size();
+							for (size_t i = 0; i < size2; i++) {
+								if (iter2->second[i].id == entity_id1) {
+									overlapMap[entity_id2].erase(iter2->second.begin() + i);
 
-								OverlapColliderEvent event(entity_id1, entity_id2, type);
-								EventDispatcher::SendEvent(event);
-								break;
+									MonoFunctionType type;
+									if (collider2.isTrigger)
+										type = MonoFunctionType::TRIGGER_EXIT;
+									else
+										type = MonoFunctionType::COLLISION_EXIT;
+
+									OverlapColliderEvent event(entity_id2, entity_id1, type);
+									EventDispatcher::SendEvent(event);
+									break;
+								}
 							}
 						}
 					}
-					const auto& iter2 = overlapMap.find(entity_id2);
-					if (iter2 != overlapMap.end()) {
-						size_t size2 = iter2->second.size();
-						for (size_t i = 0; i < size2; i++) {
-							if (iter2->second[i].id == entity_id1) {
-								overlapMap[entity_id2].erase(iter2->second.begin() + i);
 
-								MonoFunctionType type;
-								if (collider2.isTrigger)
-									type = MonoFunctionType::TRIGGER_EXIT;
-								else
-									type = MonoFunctionType::COLLISION_EXIT;
 
-								OverlapColliderEvent event(entity_id2, entity_id1, type);
-								EventDispatcher::SendEvent(event);
-								break;
-							}
-						}
-					}
 				}
-
-
 			}
 		}
 	}

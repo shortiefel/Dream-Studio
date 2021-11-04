7836a6bc26a2b0dcf4ce8f147e68187c4b7a1b49
Felicia <weilingfelicia.tan@digipen.edu>
2021-11-04 22:52:25 +0800

    relink of fmod

---
+++ GAM200/Engine/Engine/Code/ECS/Component/Sound/SoundComponent.cpp
@@ -0,0 +1,15 @@
+/* Start Header**********************************************************************************/
+/*
+@file    SoundComponent.cpp
+@author  Tan Wei Ling Felicia	 weilingfelicia.tan@digipen.edu
+@date    04/10/2021
+@brief
+
+
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **********************************************************************************/

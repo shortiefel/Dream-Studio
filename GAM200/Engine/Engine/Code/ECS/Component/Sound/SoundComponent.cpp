740a2a7de816c225dbc37a502f55f98871435d5a
Felicia <weilingfelicia.tan@digipen.edu>
2021-11-05 09:07:28 +0800

    update on sound

--- GAM200/Engine/Engine/Code/ECS/Component/Sound/SoundComponent.cpp
+++ GAM200/Engine/Engine/Code/ECS/Component/Sound/SoundComponent.cpp
@@ -1,50 +1,52 @@
-/* Start Header**********************************************************************************/
-/*
-@file    SoundComponent.cpp
-@author  Tan Wei Ling Felicia	 weilingfelicia.tan@digipen.edu
-@date    04/10/2021
-@brief
-
-
-
-Copyright (C) 2021 DigiPen Institute of Technology.
-Reproduction or disclosure of this file or its contents
-without the prior written consent of DigiPen Institute of
-Technology is prohibited.
-*/
-/* End Header **********************************************************************************/
-
-#include "Engine/Header/ECS/Component/Sound/SoundComponent.hpp"
-
-namespace Engine {
-
-	SoundImplementation::SoundImplementation() {
-
-		fmodSystem = NULL;
-	}
-
-	SoundImplementation::~SoundImplementation() {
-
-	}
-
-	void SoundImplementation::update() {
-
-	}
-
-	void SoundComponent::Init(SoundImplementation* soundImplementation) {
-
-		soundImplementation = new SoundImplementation;
-	}
-
-	void SoundComponent::Update(SoundImplementation* soundImplementation) {
-
-		soundImplementation->update();
-	}
-
-	void SoundComponent::LoadSound(const std::string& soundName, bool b3d = true, bool slooping = false, bool sStream = false) {
-
-		SoundImplementation* soundImplementation;
-		soundImplementation = new SoundImplementation;
-
-	}
-}
+///* Start Header**********************************************************************************/
+///*
+//@file    SoundComponent.cpp
+//@author  Tan Wei Ling Felicia	 weilingfelicia.tan@digipen.edu
+//@date    04/10/2021
+//@brief
+//
+//
+//
+//Copyright (C) 2021 DigiPen Institute of Technology.
+//Reproduction or disclosure of this file or its contents
+//without the prior written consent of DigiPen Institute of
+//Technology is prohibited.
+//*/
+///* End Header **********************************************************************************/
+//
+//#include "Engine/Header/ECS/Component/Sound/SoundComponent.hpp"
+//
+//namespace Engine {
+//
+//	SoundImplementation::SoundImplementation() {
+//
+//		fmodSystem = NULL;
+//	}
+//
+//	SoundImplementation::~SoundImplementation() {
+//
+//	}
+//
+//	void SoundImplementation::update() {
+//
+//	}
+//
+//	void SoundComponent::Init(SoundImplementation* soundImplementation) {
+//
+//		soundImplementation = new SoundImplementation;
+//	}
+//
+//	void SoundComponent::Update(SoundImplementation* soundImplementation) {
+//
+//		soundImplementation->update();
+//	}
+//
+//	void SoundComponent::LoadSound(const std::string& soundName, bool b3d = true, bool slooping = false, bool sStream = false) {
+//
+//		SoundImplementation* soundImplementation;
+//		soundImplementation = new SoundImplementation;
+//
+//		auto soundFind = soundImplementation->sMap.find(soundName);
+//
+//	}
+//}

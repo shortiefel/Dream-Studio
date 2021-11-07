c79d576807f66c4c499aed754aff4880ff1c09c5
wangao0416 <72902064+wangao0416@users.noreply.github.com>
2021-11-08 01:42:35 +0800

    add car controller and spawner

---
+++ GAM200/Editor/Assets/Scripts/CarController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class CarController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private float power = 5;
+
+    private float torque = 0.5f;
+
+    private float maxSpeed = 5;
+
+    private Vector2 movementVector;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 movementInput)
+    {
+        this.movementVector = movementInput;
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            rb.AddForce(movementVector.y * transform.up * power);
+        }
+        //Debug.Log(rb.inertia);
+        rb.AddTorque(movementVector.x * rb.inertia * 45);
+
+    }
+}
---
+++ GAM200/Editor/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class CarSpawner : MonoBehaviour
+{
+    public Prefab[] carPrefabs;
+
+    private void Start()
+    {
+        Instantiate(SelectACarPrefab(), transform);
+    }
+
+    private Prefab SelectACarPrefab()
+    {
+        var randomIndex = Random.Range(0, carPrefabs.Length);
+        return carPrefabs[randomIndex];
+    }
+}

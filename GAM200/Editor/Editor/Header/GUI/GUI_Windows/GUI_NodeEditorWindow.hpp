1f38ceef7ef72fd45a5714786caf23c5177d7e14
Felicia <weilingfelicia.tan@digipen.edu>
2021-11-24 19:37:36 +0800

    start of node editor

---
+++ GAM200/Editor/Editor/Header/GUI/GUI_Windows/GUI_NodeEditorWindow.hpp
@@ -0,0 +1,20 @@
+/* Start Header**********************************************************************************/
+/*
+@file			GUI_NodeEditorWindow.hpp
+@author	Tan Wei Ling Felicia	weilingfelicia.tan@digipen.edu	100%
+@date		24/11/2021
+\brief
+
+
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **********************************************************************************/
+
+#ifndef NODE_EDITOR_H
+#define NODE_EDITOR_H
+
+#endif

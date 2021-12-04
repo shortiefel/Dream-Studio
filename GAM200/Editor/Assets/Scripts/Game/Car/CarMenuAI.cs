808c19819f873731dc14ff5761cc0d59236bdd7a
Jw <jianwen123321@hotmail.com>
2021-12-04 18:05:16 +0800

    Menu Car

--- GAM200/Editor/Assets/Scripts/Game/Car/CarMenuAI.cs
+++ GAM200/Editor/Assets/Scripts/Game/Car/CarMenuAI.cs
@@ -4,37 +4,40 @@ using System;
 
 public class CarMenuAI : MonoBehaviour
 {
+    private Vector2 startPosition;
+    private Vector2 endPosition;
 
-    //[SerializeField]
-    private  Vector2 startPosition, endPosition;
+    private bool type;
 
-    private Rigidbody2D rb;
+    //private Rigidbody2D rb;
 
-    private float power;
-    private float maxSpeed;
-    private float turningFactor; //Slow down when turning
-
-    public void StartCarMenu()
+    public override void Start()
     {
-        startPosition.x = -89.4f;
-        startPosition.y = 13.4f;
-        endPosition.x = -40.3f;
-        endPosition.y = 55.8f;
+        //rb = GetComponent<Rigidbody2D>();
+        if (transform.position.x < -90)
+            type = true;
+        else
+            type = false;
+
+        if (type)
+            startPosition = new Vector2(-100f, 4.2f);
+        else
+            startPosition = new Vector2(12f, 65f);
+
     }
 
-    public void UpdateCarMenu()
+    public override void Update()
     {
-        if (rb.velocity.magnitude < maxSpeed)
+        transform.position += transform.up * 20f * Time.deltaTime;
+
+        if (type && transform.position.y > 70f ||
+            !type && transform.position.x > 100f)
         {
-            rb.AddForce(transform.up * power * turningFactor);
+            transform.position = startPosition;
         }
-    }
 
-    private void EndCarMenu()
-    {
-        Console.WriteLine("Before Notify ");
-        Destroy(gameObject);
-        Console.WriteLine("After delete ");
+
+
     }
 
 }

863e7a36676fde04aeca022539d6177f66990a92
Jw <jianwen123321@hotmail.com>
2021-12-05 17:00:51 +0800

    DrawRoad and Delete button

---
+++ GAM200/Editor/Assets/Scripts/Game/UI/ButtonRoad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+public class ButtonRoad : MonoBehaviour
+{
+    UI buttonUI;
+    bool result;
+    public override void Start()
+    {
+        buttonUI = GetComponent<UI>();
+
+        if (transform.entityId == GameObject.Find("DrawRoad").GetComponent<Transform>().entityId)
+        {
+            result = true;
+
+        }
+        else
+        {
+            result = false;
+        }
+
+    }
+
+    public override void OnMouseEnter()
+    {
+        buttonUI.color = new Color(1f, 0f, 0f);
+    }
+
+    public override void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(MouseCode.Left))
+        {
+            //SceneManager.LoadScene("MainMenu");
+            if (result)
+                Debug.Log("Change to draw");
+            else
+                Debug.Log("Change to delete");
+        }
+    }
+
+    public override void OnMouseExit()
+    {
+        buttonUI.color = new Color(1f, 1f, 1f);
+    }
+}

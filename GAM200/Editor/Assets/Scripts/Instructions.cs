1ed3743c22d070414243b0aff3b418dfc3ca0186
wangao0416 <72902064+wangao0416@users.noreply.github.com>
2021-12-06 21:10:01 +0800

    something

---
+++ GAM200/Editor/Assets/Scripts/Instructions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Instructions : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Transform trans;
+
+    public override void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        trans = GetComponent<Transform>();
+    }
+}

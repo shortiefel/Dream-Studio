42fc018ee93aeb9aea7313f3a8722e318cdef5ef
wangao0416 <72902064+wangao0416@users.noreply.github.com>
2022-01-29 17:11:53 +0800

    update

--- GAM200/Editor/Assets/Scripts/Game/Road/AdjacencyGraph.cs
+++ GAM200/Editor/Assets/Scripts/Game/Road/AdjacencyGraph.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class AdjacenyGraph
+public class AdjacencyGraph
 {
     Dictionary<Vertex, List<Vertex>> adjacencyDictionary = new Dictionary<Vertex, List<Vertex>>();
 

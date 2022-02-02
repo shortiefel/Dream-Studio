ee298781ea983302443f7f7d7114146141ad2bc0
Felicia <weilingfelicia.tan@digipen.edu>
2022-02-02 19:33:29 +0800

    edited options scene

--- GAM200/Editor/Assets/Scripts/Menu/GameScene/MusicPlayer.cs
+++ GAM200/Editor/Assets/Scripts/Menu/GameScene/MusicPlayer.cs
@@ -4,12 +4,14 @@ public class MusicPlayer : MonoBehaviour
 {
     AudioSource t1;
     AudioSource t2;
+    Text textComp;
 
     public override void Start()
     {
        GetComponent<AudioSource>().Play();
         t1 = GetComponent<AudioSource>();
         t2 = GameObject.Find("Camera").GetComponent<AudioSource>();
+        textComp = GameObject.Find("VolumeAdjust").GetComponent<Text>();
 
 
         Debug.Log("oshfddosfhoidsfjhoi");
@@ -29,6 +31,8 @@ public class MusicPlayer : MonoBehaviour
         {
             t2.mute = false;
         }
+
+        textComp.text = t1.volume.ToString();
     }
 
 }

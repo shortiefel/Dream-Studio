bf0a7a08d212d6125f2fa6ad90446f918b4ce7e8
Felicia <weilingfelicia.tan@digipen.edu>
2022-02-02 22:57:10 +0800

    option scene more crap

--- GAM200/Editor/Assets/Scripts/Menu/GameScene/MusicPlayer.cs
+++ GAM200/Editor/Assets/Scripts/Menu/GameScene/MusicPlayer.cs
@@ -4,15 +4,13 @@ public class MusicPlayer : MonoBehaviour
 {
     AudioSource t1;
     AudioSource t2;
-    Text textComp;
 
     public override void Start()
     {
        GetComponent<AudioSource>().Play();
         t1 = GetComponent<AudioSource>();
         t2 = GameObject.Find("Camera").GetComponent<AudioSource>();
-        textComp = GameObject.Find("VolumeAdjust").GetComponent<Text>();
-
+    
 
         Debug.Log("oshfddosfhoidsfjhoi");
     }
@@ -32,7 +30,7 @@ public class MusicPlayer : MonoBehaviour
             t2.mute = false;
         }
 
-        textComp.text = t1.volume.ToString();
+        
     }
 
 }
---
+++ GAM200/Editor/Assets/Scripts/Menu/Options/OptionsMusic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class OptionsMusic : MonoBehaviour
+{
+  
+    AudioSource volObject, BGM , SFX;
+    Text textComp;
+    bool BGMMute, SFXMute;
+    Transform MuteBGMX;
+    Transform MuteSFXX;
+
+
+    public override void Start()
+    {
+        //GetComponent<AudioSource>().Play();
+        //BGM = GameObject.Find("Camera").GetComponent<AudioSource>();
+
+        volObject = GameObject.Find("VolumeAdjust").GetComponent<AudioSource>();
+        textComp = GetComponent<Text>();
+        if (textComp != null)
+            textComp.text = Convert.ToString(volObject.volume);
+        MuteBGMX = GameObject.Find("BGMMuteX").GetComponent<Transform>();
+        MuteSFXX = GameObject.Find("SFXMuteX").GetComponent<Transform>();
+
+        Disable<Transform>(MuteBGMX);
+        Disable<Transform>(MuteSFXX);
+
+        BGMMute = false;
+        SFXMute = false;
+
+        Debug.Log("oshfddosfhoidsfjhoi");
+    }
+
+    public override void Update()
+    {
+        textComp.text = Convert.ToString(volObject.volume);
+
+    }
+
+    public override void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(MouseCode.Left))
+        {
+            if (BGMMute)
+            {
+                Enable<Transform>(MuteBGMX);
+              
+                
+            }
+            else
+            {
+                Disable<Transform>(MuteBGMX);
+            }
+        }
+    }
+
+}
+

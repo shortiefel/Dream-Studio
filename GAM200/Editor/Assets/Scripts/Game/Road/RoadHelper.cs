105fd75d2b0714039cc26cad9ceb6291ca1f0754
wangao0416 <72902064+wangao0416@users.noreply.github.com>
2022-02-03 19:57:26 +0800

    HELP

--- GAM200/Editor/Assets/Scripts/Game/Road/RoadHelper.cs
+++ GAM200/Editor/Assets/Scripts/Game/Road/RoadHelper.cs
@@ -12,7 +12,41 @@ public class RoadHelper : MonoBehaviour
 
     float approximateThresholdCorner = 0.3f;
 
-    private Marker incomming, outgoing;
+    private Marker incoming, outgoing;
+
+    public override void Start()
+    {
+        Waypoint wp = GetComponent<Waypoint>();
+
+        //The list hold list of vector2 (each List<Vector2> is a possible path/route the car can take
+        //The points added should be with reference to the center 0, 0
+        //Order is how the list of points (shown in the inspector) will be cut 
+        //The first value currently is 2 so only 2 position is considered as one route
+        //The Console.WriteLine below will display how the route are spilt up in the console
+        //The numbers in the order should add up to the number of points your have
+
+        //You can view the TestScene.scene to see how to directly add the points and order in or another way is to add it using the inspector
+
+        //TLDR
+        //points = all possible waypoint the car can go to
+        //order = how to group the points (the number of element = number of route) (the total value of all order should be = number of waypoints)
+
+
+        List<List<Vector2>> listOfWaypoints = wp.GetWaypoints();
+
+        foreach (var i in listOfWaypoints)
+        {
+            //Console.WriteLine("Current set");
+            foreach (var t in i)
+            {
+                //Console.WriteLine(t);
+                List<Marker> carMarkers = t.ToList();
+            }
+
+            //Console.WriteLine("\nNext Set \n");
+        }
+
+    }
 
     public virtual Marker GetpositioForPedestrianToSpwan(Vector2 structurePosition)
     {
@@ -26,7 +60,7 @@ public class RoadHelper : MonoBehaviour
 
     public virtual Marker GetPositioForCarToEnd(Vector2 previousPathPosition)
     {
-        return incomming;
+        return incoming;
     }
 
     protected Marker GetClosestMarkeTo(Vector2 structurePosition, List<Marker> carMarkers, bool isCorner = false)

83d800fafdc6a4ae8de09b36f3c2bf9037a8bb74
Felicia <weilingfelicia.tan@digipen.edu>
2022-02-03 15:08:57 +0800

    changes to credits, rolling not done

---
+++ GAM200/Editor/Assets/Scripts/Menu/Credits/RollingCredits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class RollingCredits : MonoBehaviour
+{
+
+    Camera cam;
+    float timer, startHeight, EndHeight;
+    Transform Rolling;
+
+    Vector2 targetPosition;
+    Vector2 startPosition;
+
+    public override void Start()
+    {
+        timer = 0;
+        Rolling = GameObject.Find("RollingText").GetComponent<Transform>();
+
+        cam = GetComponent<Camera>();
+        startHeight = cam.height;
+        EndHeight = 10f;
+    }
+
+    public override void Update()
+    {
+        timer += Time.unscaledDeltaTime;
+        float h = cam.height = Mathf.Lerp(startHeight, EndHeight, timer);
+        //if (timer > 0)
+        //{
+            transform.position = new Vector2(Mathf.Lerp(startPosition.x, targetPosition.x, timer), Mathf.Lerp(startPosition.y, targetPosition.y, timer));
+            
+        //}
+        Debug.Log(h);
+       
+
+
+    }
+}

7bee6ec65acc057dc78f5a8b4532caaf502a05d3
Felicia <weilingfelicia.tan@digipen.edu>
2022-02-04 20:53:15 +0800

    skipping frm tut to main game

---
+++ GAM200/Editor/Assets/Scripts/Game/Tutorial/Skipping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+public class Skipping : MonoBehaviour
+{
+    Text text;
+    Texture texture;
+
+    public override void Start()
+    {
+        Console.WriteLine("going into game scene");
+
+        text = GetComponent<Text>();
+        texture = GameObject.Find("SKIPbtn").GetComponent<Texture>();
+    }
+
+    public override void OnMouseEnter()
+    {
+        text.color = new Color(0f, 0f, 0f);
+        texture.color = new Color(1f, 1f, 1f);
+    }
+
+    public override void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(MouseCode.Left))
+        {
+            Console.WriteLine("skipping");
+            SceneManager.LoadScene("NewGame");
+        }
+    }
+
+    public override void OnMouseExit()
+    {
+        text.color = new Color(1f, 1f, 1f);
+        texture.color = new Color(0f, 0f, 0f);
+    }
+}

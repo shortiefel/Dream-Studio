68498a26242540dd03abaa90eea14007b47925ba
Jw <jianwen123321@hotmail.com>
2022-02-05 23:10:26 +0800

    Works fine for me

--- GAM200/Editor/Assets/Scripts/Menu/GameScene/MusicPlayer.cs
+++ GAM200/Editor/Assets/Scripts/Menu/GameScene/MusicPlayer.cs
@@ -11,25 +11,7 @@ public class MusicPlayer : MonoBehaviour
 
     }
 
-    public override void Update()
-    { 
-      if (Input.GetKeyDown(KeyCode.A))
-        {
-            //AudioSource.SetGroup_Mute(AudioGroup.Music, true);
-            AudioSource.SetGroup_Volume(AudioGroup.Music, 100f);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            //AudioSource.SetGroup_Mute(AudioGroup.Music, false);
-            AudioSource.SetGroup_Volume(AudioGroup.Music, 10f);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            //t2.mute = false;
-        }
-
-        
-    }
+    
 
 }
 

dca088243cee495531ac5aac6ed87692ac47c544
Jw <jianwen123321@hotmail.com>
2022-02-06 18:51:28 +0800

    Update

--- GAM200/Editor/Assets/Scripts/Game/Road/AdjacencyGraph.cs
+++ GAM200/Editor/Assets/Scripts/Game/Road/AdjacencyGraph.cs
@@ -40,7 +40,12 @@ public class AdjacencyGraph
 
     private Vertex GetVertexAt(Vector2 position)
     {
-        return adjacencyDictionary.Keys.FirstOrDefault(x => CompareVertices(position, x.Position));
+        foreach(var x in adjacencyDictionary.Keys)
+        {
+            if (CompareVertices(position, x.Position)) return x;
+        }
+        return null;
+        //return adjacencyDictionary.Keys.FirstOrDefault(x => CompareVertices(position, x.Position));
     }
 
     private bool CompareVertices(Vector2 position1, Vector2 position2)
@@ -78,10 +83,21 @@ public class AdjacencyGraph
         }
         if (adjacencyDictionary.ContainsKey(v1))
         {
-            if (adjacencyDictionary[v1].FirstOrDefault(x => x == v2) == null)
+            bool found = false;
+            foreach (var x in adjacencyDictionary[v1])
             {
-                adjacencyDictionary[v1].Add(v2);
+                if (x == v2)
+                {
+                    found = true;
+                    break;
+                }
             }
+            
+            if(!found) adjacencyDictionary[v1].Add(v2);
+            //if (adjacencyDictionary[v1].FirstOrDefault(x => x == v2) == null)
+            //{
+            //    adjacencyDictionary[v1].Add(v2);
+            //}
         }
         else
         {

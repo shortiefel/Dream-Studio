21bf8b34a6aa46564d722b566843957164171b4c
Felicia <weilingfelicia.tan@digipen.edu>
2022-02-10 13:13:51 +0800

    tut game scene pop

---
+++ GAM200/Editor/Assets/Scripts/Menu/PopUps/TrafficLightPopIntro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class TrafficLightPopIntro : MonoBehaviour
+{
+
+
+    public override void Start()
+    {
+
+    }
+
+    public override void Update()
+    {
+       
+    }
+}

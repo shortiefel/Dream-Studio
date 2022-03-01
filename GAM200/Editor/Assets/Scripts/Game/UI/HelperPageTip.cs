c1bb5218abc631ccc43bd4be9ea794183037e070
Felicia <weilingfelicia.tan@digipen.edu>
2022-03-01 20:43:45 +0800

    helper pagetip

---
+++ GAM200/Editor/Assets/Scripts/Game/UI/HelperPageTip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class HelperPageTip : MonoBehaviour
+{
+
+    public override void Update()
+    {
+        if (transform.isActive && Input.GetMouseButtonDown(MouseCode.Left))
+        {
+            Disable<Transform>(transform);
+            Time.timeScale = 1f;
+
+            Enable<Transform>(GameObject.Find("MoneyText").GetComponent<Transform>());
+            Enable<Transform>(GameObject.Find("CounterText").GetComponent<Transform>());
+            Enable<Transform>(GameObject.Find("Displaybtn").GetComponent<Transform>());
+        }
+    }
+}

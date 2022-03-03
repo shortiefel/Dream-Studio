1a6cbbaca9bd7df8006676229a9a0afecfc7763f
Jw <jianwen123321@hotmail.com>
2022-03-03 11:08:52 +0800

    Stop time and speed up time

---
+++ GAM200/Editor/Assets/Scripts/Game/GameValues/TimeSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class TimeSystem : MonoBehaviour
+{
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Time.timeScale = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 3f;
+        }
+    }
+}

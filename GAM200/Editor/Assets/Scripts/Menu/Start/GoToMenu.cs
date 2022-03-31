b19c9fcd2ffe1cbb876b71b9cfd7332e35a74797
jianwen12390 <48422533+jianwen12390@users.noreply.github.com>
2022-03-31 11:09:14 +0800

    Receipt disabled, fix Intro scene error

--- GAM200/Editor/Assets/Scripts/Menu/Start/GoToMenu.cs
+++ GAM200/Editor/Assets/Scripts/Menu/Start/GoToMenu.cs
@@ -1,26 +1,14 @@
 ﻿public class GoToMenu : MonoBehaviour
 {
-    Text text;
-    public override void Start()
-    {
-        text = GetComponent<Text>();
-        text.color = new Color(0f, 0f, 0f);
-    }
+    //Text text;
+ 
 
-    public override void OnMouseEnter()
-    {
-        text.color = new Color(1f, 1f, 1f);
-    }
-    public override void OnMouseOver()
+    public override void Update()
     {
         if (Input.GetMouseButtonDown(MouseCode.Left))
         {
             Time.timeScale = 1;
             SceneManager.LoadScene("MainMenu");
         }
-    }
-    public override void OnMouseExit()
-    {
-        text.color = new Color(0f, 0f, 0f);
     }
 }

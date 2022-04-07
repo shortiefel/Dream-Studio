c7e56c12b7a4a607a542c0b7b8a40341091381da
Felicia <weilingfelicia.tan@digipen.edu>
2022-04-07 19:42:28 +0800

    tut

---
+++ GAM200/Editor/Assets/Scripts/Game/Tutorial/TutorialInstructions.cs
@@ -0,0 +1,52 @@
+﻿public class TutorialInstructions : MonoBehaviour
+{
+
+    /************** Instructions scripts (1-21)***********/
+    InstructionsClick instructions1Script;
+    InstructionsClick instructions2Script;
+    InstructionsClick instructions3Script;
+    InstructionsClick instructions4Script;
+    InstructionsClick instructions5Script;
+    InstructionsClick instructions6Script;
+    InstructionsClick instructions7Script;
+    InstructionsClick instructions8Script;
+    InstructionsClick instructions9Script;
+    InstructionsClick instructions10Script;
+    InstructionsClick instructions11Script;
+    InstructionsClick instructions12Script;
+    InstructionsClick instructions13Script;
+    InstructionsClick instructions14Script;
+    InstructionsClick instructions15Script;
+    InstructionsClick instructions16Script;
+    InstructionsClick instructions17Script;
+    InstructionsClick instructions18Script;
+    InstructionsClick instructions19Script;
+    InstructionsClick instructions20Script;
+    InstructionsClick instructions21Script;
+
+    GameObject go1;
+    GameObject go2;
+    GameObject go3;
+    GameObject go4;
+    GameObject go5;
+    GameObject go6;
+    GameObject go7;
+    GameObject go8;
+    GameObject go9;
+    GameObject go10;
+    GameObject go11;
+    GameObject go12;
+    GameObject go13;
+    GameObject go14;
+    GameObject go15;
+    GameObject go16;
+    GameObject go17;
+    GameObject go18;
+    GameObject go19;
+    GameObject go20;
+    GameObject go21;
+
+
+
+
+}

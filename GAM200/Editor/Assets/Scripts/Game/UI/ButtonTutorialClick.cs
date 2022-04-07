abc560bd2d82c96e4b5fbca0b164dc175673fca3
Felicia <weilingfelicia.tan@digipen.edu>
2022-04-07 18:48:54 +0800

    music asset updated to all scenes

--- GAM200/Editor/Assets/Scripts/Game/UI/ButtonTutorialClick.cs
+++ GAM200/Editor/Assets/Scripts/Game/UI/ButtonTutorialClick.cs
@@ -14,21 +14,31 @@ public class ButtonTutorialClick : MonoBehaviour
 
     public override void Start()
     {
-        buttonTut = GameObject.Find("TutorialRoad").GetComponent<ButtonTutorial>();
+        buttonTut = GameObject.Find("Displaybtn").GetComponent<ButtonTutorial>();
 
-
+        
         if (entityId == GameObject.Find("DrawRoad").GetComponent<Transform>().entityId)
         {
             tbt = TutButtonType.Draw;
             tooltipOffset = transform.position + new Vector2(-3.60f, 7.70f);
-            //active = true;
+        }
+
+        else if (entityId == GameObject.Find("DrawRoadWhite").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.Draw;
+            tooltipOffset = transform.position + new Vector2(-3.60f, 7.70f);
         }
 
         else if (entityId == GameObject.Find("RemoveRoad").GetComponent<Transform>().entityId)
         {
             tbt = TutButtonType.Remove;
             tooltipOffset = transform.position + new Vector2(-6.00f, 7.70f);
-            //active = true;
+        }
+
+        else if (entityId == GameObject.Find("RemoveRoadWhite").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.Remove;
+            tooltipOffset = transform.position + new Vector2(-6.00f, 7.70f);
         }
 
         else if (entityId == GameObject.Find("RemoveCar").GetComponent<Transform>().entityId)
@@ -37,55 +47,88 @@ public class ButtonTutorialClick : MonoBehaviour
             tooltipOffset = transform.position + new Vector2(-8.80f, 7.70f);
         }
 
+        else if (entityId == GameObject.Find("RemoveCarWhite").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.RemoveCar;
+            tooltipOffset = transform.position + new Vector2(-8.80f, 7.70f);
+        }
+
+        else if (entityId == GameObject.Find("ERPbtn").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.ERP;
+            tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
+        }
+
+        else if (entityId == GameObject.Find("ERPbtnWhite").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.ERP;
+            tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
+        }
+
+        else if (entityId == GameObject.Find("TrafficLight").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.TrafficLight;
+            tooltipOffset = transform.position + new Vector2(-10.10f, 7.70f);
+        }
+
+        else if (entityId == GameObject.Find("TrafficLightWhite").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.TrafficLight;
+            tooltipOffset = transform.position + new Vector2(-10.10f, 7.70f);
+        }
+
+        //else if (entityId == GameObject.Find("Displaybtn").GetComponent<Transform>().entityId)
+        //{
+        //    //Debug.Log("selected1");
+        //    bt = ButtonType.Display;
+        //    tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
+        //}
+        //
+        //else if(entityId == GameObject.Find("DisplaybtnWhite").GetComponent<Transform>().entityId)
+        //{
+        //    bt = ButtonType.Display;
+        //    tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
+        //}
+
         else if (entityId == GameObject.Find("PlaceHospital").GetComponent<Transform>().entityId)
         {
             tbt = TutButtonType.PlaceHospital;
             tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
         }
 
-        //else if (entityId == GameObject.Find("RemoveRoadGlow").GetComponent<Transform>().entityId)
-        //{
-        //    tbt = TutButtonType.Remove;
-        //    type = false;
-        //    //active = false;
-        //}
-        //else if (entityId == GameObject.Find("DrawRoadGlow").GetComponent<Transform>().entityId)
-        //{
-        //    tbt = TutButtonType.Draw;
-        //    type = false;
-        //    //active = false;
-        //}
+        else if (entityId == GameObject.Find("PlaceOffice").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.PlaceOffice;
+            tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
+        }
 
+        else if (entityId == GameObject.Find("PlacePark").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.PlacePark;
+            tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
+        }
 
-        //if (entityId == GameObject.Find("Displaybtn").GetComponent<Transform>().entityId)
-        //{
-        //    //Debug.Log("selected1");
-        //    tbt = TutButtonType.Display;
-        //    type = true;
-        //    //Debug.Log("selected2");
-        //    //active = true;
-        //}
+        else if (entityId == GameObject.Find("PlaceMall").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.PlaceMall;
+            tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
+        }
 
-        //if (entityId == GameObject.Find("DisplayGlow").GetComponent<Transform>().entityId)
-        //{
-        //    //Debug.Log("selected1");
-        //    tbt = TutButtonType.Display;
-        //    type = true;
-        //    //Debug.Log("selected2");
-        //    //active = true;
-        //}
+        else if (entityId == GameObject.Find("PlacePoliceStation").GetComponent<Transform>().entityId)
+        {
+            tbt = TutButtonType.PlacePoliceStation;
+            tooltipOffset = transform.position + new Vector2(-2.40f, 7.70f);
+        }
+      
 
-
-
-        //tooltipOffset = transform.position + new Vector2(-2f, 6.5f);
 
     }
     public override void OnMouseOver()
     {
 
         //if (!transform.isActive) return;
-        if (transform.isActive)
-        {
+       // if (transform.isActive)
+        //{
             switch (tbt)
             {
                 //case TutButtonType.Display:
@@ -134,7 +177,28 @@ public class ButtonTutorialClick : MonoBehaviour
                         //tooltipText.text = "Remove Car";
                         break;
                     }
-                case TutButtonType.PlaceHospital:
+
+            case TutButtonType.ERP:
+                {
+                    if (Input.GetMouseButtonDown(MouseCode.Left))
+                        buttonTut.CallFunctionTut(tbt);
+
+                    buttonTut.SetToolTips(true, tooltipOffset, "ERP");
+                    //Enable<Transform>(tooltipTrans);
+                    //tooltipText.text = "ERP";
+                    break;
+                }
+            case TutButtonType.TrafficLight:
+                {
+                    if (Input.GetMouseButtonDown(MouseCode.Left))
+                        buttonTut.CallFunctionTut(tbt);
+
+                    buttonTut.SetToolTips(true, tooltipOffset, "TrafficLight");
+                    //Enable<Transform>(tooltipTrans);
+                    //tooltipText.text = "TrafficLight";
+                    break;
+                }
+            case TutButtonType.PlaceHospital:
                     {
                         if (Input.GetMouseButtonDown(MouseCode.Left))
                         {
@@ -147,17 +211,73 @@ public class ButtonTutorialClick : MonoBehaviour
 
                         break;
                     }
+                case TutButtonType.PlaceOffice:
+                    {
+                        if (Input.GetMouseButtonDown(MouseCode.Left))
+                        {
+                            buttonTut.CallFunctionTut(tbt);
+                        }
+
+                        buttonTut.SetToolTips(true, tooltipOffset, "Place Office");
+                        //Enable<Transform>(tooltipTrans);
+                        //tooltipText.text = "Place Office";
+
+                        break;
+                    }
+                case TutButtonType.PlacePark:
+                    {
+                        if (Input.GetMouseButtonDown(MouseCode.Left))
+                        {
+                            buttonTut.CallFunctionTut(tbt);
+                        }
+
+                        buttonTut.SetToolTips(true, tooltipOffset, "Place Park");
+                        //Enable<Transform>(tooltipTrans);
+                        //tooltipText.text = "Place Park";
+
+                        break;
+                    }
+                case TutButtonType.PlaceMall:
+                    {
+                        if (Input.GetMouseButtonDown(MouseCode.Left))
+                        {
+                            buttonTut.CallFunctionTut(tbt);
+                        }
+
+                        buttonTut.SetToolTips(true, tooltipOffset, "Place Mall");
+                        //Enable<Transform>(tooltipTrans);
+                        //tooltipText.text = "Place Mall";
+
+                        break;
+                    }
+                case TutButtonType.PlacePoliceStation:
+                    {
+                        if (Input.GetMouseButtonDown(MouseCode.Left))
+                        {
+                            buttonTut.CallFunctionTut(tbt);
+                        }
+
+                        buttonTut.SetToolTips(true, tooltipOffset, "Place Police Station");
+
+                        break;
+                    }
 
                 default:
 
                     break;
             }
 
-        }
+        //}
+    }
+
+    public override void OnMouseEnter()
+    {
+        InputManager.allowBuilding = false;
     }
 
     public override void OnMouseExit()
     {
+        InputManager.allowBuilding = true;
         buttonTut.SetToolTips(false, Vector2.zero);
 
         //if (!transform.isActive) return;

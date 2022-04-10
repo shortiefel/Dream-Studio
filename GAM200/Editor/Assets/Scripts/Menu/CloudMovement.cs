010ce2073e5ba2b040685ad989e771a6e6b74918
jianwen12390 <48422533+jianwen12390@users.noreply.github.com>
2022-04-11 00:46:56 +0800

    Cloud push

---
+++ GAM200/Editor/Assets/Scripts/Menu/CloudMovement.cs
@@ -0,0 +1,24 @@
+﻿
+public class CloudMovement : MonoBehaviour
+{
+    float speed;
+    float yPos;
+
+    public override void Start()
+    {
+        speed = Random.Range(1, 5) + (float)Random.Range(1, 5)/10f;
+        yPos = transform.position.y;
+
+    }
+
+    public override void Update()
+    {
+        transform.position += new Vector2(speed * Time.deltaTime, 0);
+
+        if (transform.position.x > 17)
+        {
+            speed = Random.Range(1, 5) + (float)Random.Range(1, 5) / 10f;
+            transform.position = new Vector2(-17, yPos);
+        }
+    }
+}

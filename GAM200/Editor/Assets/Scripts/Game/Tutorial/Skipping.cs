704fde3e3fbdcce6e2ee4918a6f758db948395a2
jianwen12390 <48422533+jianwen12390@users.noreply.github.com>
2022-04-11 01:16:55 +0800

    Skip tutorial and delete scene

--- GAM200/Editor/Assets/Scripts/Game/Tutorial/Skipping.cs
+++ GAM200/Editor/Assets/Scripts/Game/Tutorial/Skipping.cs
@@ -2,27 +2,27 @@
 public class Skipping : MonoBehaviour
 {
     Text text;
-    Texture texture;
+    //Texture texture;
 
     public override void Start()
     {
-        Console.WriteLine("going into game scene");
+        //Console.WriteLine("going into game scene");
 
         text = GetComponent<Text>();
-        texture = GameObject.Find("SKIPbtn").GetComponent<Texture>();
+        //texture = GameObject.Find("SKIPbtn").GetComponent<Texture>();
     }
 
     public override void OnMouseEnter()
     {
         text.color = new Color(0f, 0f, 0f);
-        texture.color = new Color(1f, 1f, 1f);
+        //texture.color = new Color(1f, 1f, 1f);
     }
 
     public override void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(MouseCode.Left))
         {
-            Console.WriteLine("skipping");
+            //Console.WriteLine("skipping");
             SceneManager.LoadScene("NewGame");
         }
     }
@@ -30,6 +30,6 @@ public class Skipping : MonoBehaviour
     public override void OnMouseExit()
     {
         text.color = new Color(1f, 1f, 1f);
-        texture.color = new Color(0f, 0f, 0f);
+        //texture.color = new Color(0f, 0f, 0f);
     }
 }
